70728652501f616466b1080bde6cc63e40191a05
Noé <nono.beran@gmail.com>
2026-09-18 11:20:18 +0200

    Feat(drone): ajouter un 2eme drone

--- personnel/Drones/Drones/Program.cs
+++ personnel/Drones/Drones/Program.cs
@@ -17,7 +17,10 @@ namespace Drones
             // Création de la flotte de drones
             List<Drone> fleet= new List<Drone>();
             fleet.Add(new Drone(Config.AIRSPACE_WIDTH / 2, Config.AIRSPACE_HEIGHT / 2, "Joe"));
-
+            fleet.Add(new Drone(Config.AIRSPACE_WIDTH / 3, Config.AIRSPACE_HEIGHT / 3 + 10, "Mark"));
+            fleet.Add(new Drone(Config.AIRSPACE_WIDTH / 2 + 135, Config.AIRSPACE_HEIGHT / 2 - 91, "Dan"));
+            fleet.Add(new Drone(Config.AIRSPACE_WIDTH / 2 + 222, Config.AIRSPACE_HEIGHT - 222, "Bob"));
+            fleet.Add(new Drone(Config.AIRSPACE_WIDTH / 2, Config.AIRSPACE_HEIGHT - 104, "Sam"));
             // Démarrage
             Application.Run(new AirSpace(fleet));
         }
