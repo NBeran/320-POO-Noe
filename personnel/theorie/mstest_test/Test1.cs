17841982c3a350304e019523ae185e4968f935bd
Noé <nono.beran@gmail.com>
2026-09-25 11:18:08 +0200

    learn (theorie) : apprendre a faire des tests

---
+++ personnel/theorie/mstest_test/Test1.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Channels;
+using testtest;
+
+namespace mstest_test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void testThatSumOfTenAndTenIsTwenty()
+        {
+            //Arrgange
+            int x = 10;
+            int y = 10;
+            int z = -15;
+
+            //Act
+            int res = MyMath.Somme(x, y);
+            int res2 = MyMath.Somme(z, x);
+
+            //Assert
+            Assert.AreEqual(20, res);
+            Assert.AreEqual(-5, res2);
+        }
+    }
+}
---
+++ personnel/theorie/test_test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Channels;
+
+namespace testtest
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+            
+        }
+
+    }
+
+    public class MyMath
+    {
+        public static int Somme(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
