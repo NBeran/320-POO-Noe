c408773066ccd7849e535368a22b48eab44aabe2
Noé <nono.beran@gmail.com>
2026-09-25 12:14:55 +0200

    feat (pizzeria, etape 3.1) ajouter la class pizzera et lui donner ses attibuts

--- personnel/Drones/Drones/Model/Pizzeria.cs
+++ personnel/Drones/Drones/Model/Pizzeria.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Drones.Model
 {
-    internal class Class1
+    internal class Pizzeria
     {
+        private int _x;
+        private int _y;
+        private string _name;
+
+        public string Name { get => _name; private set => _name = value; }
+        public int Y { get => _y; private set => _y = value; }
+        public int X { get => _x; private set => _x = value; }
+        public Pizzeria(int x, int y, string name)
+        {
+            X = x;
+            Y = y;
+            Name = name;
+        }
+
+
     }
 }
